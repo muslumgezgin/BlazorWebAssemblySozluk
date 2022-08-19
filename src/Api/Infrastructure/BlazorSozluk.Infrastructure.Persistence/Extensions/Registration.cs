442a1e5ada5747b63de8a2e2c1e8e09747113aaf
muslum <muslum_gezgin@hotmail.com>
2022-08-19 13:33:38 +0200

    Seed Entities by using Bogus library

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Extensions/Registration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace BlazorSozluk.Infrastructure.Persistence.Extensions
+{
+    public  static class Registration
+    {
+        public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services,IConfiguration configuration)
+        {
+
+            services.AddDbContext<BlazorSozlukContext>(conf =>
+            {
+                var conStr = configuration["BlazorSozlukDbConnectionString"].ToString();
+                conf.UseSqlServer(conStr, opt =>
+                {
+                    opt.EnableRetryOnFailure();
+                });
+            });
+
+            var seedData = new SeedData();
+
+            seedData.SeedAsync(configuration).GetAwaiter().GetResult();
+
+            return services;
+
+        }
+    }
+}
+
