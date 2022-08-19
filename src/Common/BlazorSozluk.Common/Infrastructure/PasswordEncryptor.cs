2711862bf227ac5b016b7c82a42b02e1252e8bf7
muslum <muslum_gezgin@hotmail.com>
2022-08-19 13:32:20 +0200

    create encryp password mecanism on common library

---
+++ src/Common/BlazorSozluk.Common/Infrastructure/PasswordEncryptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlazorSozluk.Common.Infrastructure
+{
+    public class PasswordEncryptor
+    {
+        public static string Encrypt(string password)
+        {
+            using var md5 = MD5.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes(password);
+
+            byte[] hasBytes = md5.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hasBytes);
+        }
+    }
+}
+
