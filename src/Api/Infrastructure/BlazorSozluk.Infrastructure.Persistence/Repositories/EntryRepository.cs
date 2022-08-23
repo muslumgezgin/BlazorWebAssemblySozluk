87a2ee3944393908092abdaf78c015debe849af0
muslum <muslum_gezgin@hotmail.com>
2022-08-23 13:32:25 +0200

    Add user and enty repositories

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Repositories/EntryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Repositories
+{
+    public class EntryRepository : GenericRepository<Entry>, IEntryRepository
+    {
+        public EntryRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
+
---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BlazorSozluk.Api.Domain.Models;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository :GenericRepository<User>,IUserRepository
+    {
+      
+    }
+}
+
