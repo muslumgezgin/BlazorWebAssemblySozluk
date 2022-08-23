cfd5162cee4f4fecd6534b27856aea21127257f1
muslum <muslum_gezgin@hotmail.com>
2022-08-23 13:29:23 +0200

    Add IUser and IEntry

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using BlazorSozluk.Api.Domain.Models;
+
+namespace BlazorSozluk.Api.Application.Interfaces.Repositories;
+
+public interface IUserRepository : IGenericRepository<User>
+{
+
+}
+
