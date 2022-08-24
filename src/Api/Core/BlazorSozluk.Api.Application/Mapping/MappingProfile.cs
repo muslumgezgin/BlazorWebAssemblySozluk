42afcd388024175abc9a871a554a4688859447e0
muslum <muslum_gezgin@hotmail.com>
2022-08-24 09:58:25 +0200

    ADd Login User Command Handler and Validator to Application Layer

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Common.ViewModels.Queries;
+
+namespace BlazorSozluk.Api.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, LoginUserViewModel>().ReverseMap();
+        }
+    }
+}
+
