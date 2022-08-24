abc9773de136d20a11e60bc609c2439e65da7783
muslum <muslum_gezgin@hotmail.com>
2022-08-24 09:58:54 +0200

    add login api controller to web api

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BlazorSozluk.Common.ViewModels.RequestModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSozluk.Api.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public UserController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody]LoginUserCommand command)
+    {
+        var res = await mediator.Send(command);
+
+        return Ok(res);
+
+    }
+
+
+
+}
+
